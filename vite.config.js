fb506687a677f1a51ae233d10e83a96afcacf23d
Subramaniam, Rajalingam <RAJALINGAM.SUBRAMANIAM@OPTUM.COM>
2025-11-28 12:11:58 +0530

    Fix the issue
    Fix the issue

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,14 @@
-import {defineConfig} from 'vite';import react from '@vitejs/plugin-react';export default defineConfig({plugins:[react()],server:{port:5173}})
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: process.env.PORT || 5173,
+    host: '0.0.0.0'
+  },
+  preview: {
+    port: process.env.PORT || 5173,
+    host: '0.0.0.0'
+  }
+});
